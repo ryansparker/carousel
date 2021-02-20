7ffcd0710ccf907598add9c70d6c237ad5f0b6dc
Ryan Parker <ryansparker@gmail.com>
2021-02-20 15:20:34 -0500

    buttons working to move through slides forward and back

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ document
 
 buttonPrev.addEventListener("click", moveToPrevSlide);
 
+function hideAllSlides() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
+}
+
 function moveToNextSlide() {
+  hideAllSlides();
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
@@ -19,10 +27,12 @@ function moveToNextSlide() {
   slides[slidePosition].classList.add("carousel-item-visible");
 }
 
-// function moveToPrevSlide() {
-//   if (slidePosition === totalSlides - 1) {
-//     slidePosition = 0;
-//   } else {
-//     slidePosition--;
-//   }
-// }
+function moveToPrevSlide() {
+  hideAllSlides();
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  slides[slidePosition].classList.add("carousel-item-visible");
+}
